6d87360700e5e93d5fb7dc6823a89150d9051435
Kay Leacock <kay@ccpgsolutions.net>
2013-08-22 23:09:16 +0100

    Added code to switch between the formsets being displayed, as well as generate explanations of the various gender identities.

--- site.js
+++ site.js
@@ -14,4 +14,22 @@ $(document).ready(function(){
 		$('#form-city').val(datum.city);
 		$('#form-postcode').val(datum.postcode);
 	});
+
+	$('#btn-initial-next').click(function(e){
+		e.preventDefault();
+		$('#formset-initial').hide();
+		$('#formset-misgendered').show();
+	});
+
+	$('#form-gender-id').change(function(e){
+		if($('#form-gender-id').val() == "ftm"){
+			var explanation = "I am a man who was born into a typically female body. Throughout my life, I have been aware of the fact I am male, and I present as male in my day-to-day existence.";
+		}else if($('#form-gender-id').val() == "mtf"){
+			var explanation = "I am a woman who was born into a typically male body. Throughout my life, I have been aware of the fact I am female, and I present as female in my day-to-day existence.";
+		}else if($('#form-gender-id').val() == "genderqueer" || $('#form-gender-id').val() == "androgyne" || $('#form-gender-id').val() == "genderless" || $('#form-gender-id').val() == "genderless"){
+			var explanation = "I am a person who does not fit into the narrow two gender system used by the majority of western societies. I am neither male nor female. Though this is a concept unknown to many non-transgender people, it is important to note that the NHS and many other world health services recognise gender as a spectrum.";
+		}else if($('#form-gender-id').val() == "genderfluid"){
+			var explanation = "I am a person who does not fit into the narrow two gender system used by the majority of western societies. Though this is a concept unknown to many non-transgender people, it is important to note that the NHS and many other world health services recognise gender as a spectrum. My gender identity moves around within this spectrum, and I sometimes change the way I present my gender to the world. This helps me express this, and also helps me feel more comfortable in my body.";
+		}
+	})
 });
