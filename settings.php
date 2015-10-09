3d2c2ecaf896b0cefa26a3218b148d07623fa13a
Kat <kat@polypod.me>
2015-10-09 18:58:06 +0100

    Added letter templates to settings

--- settings.php
+++ settings.php
@@ -14,4 +14,13 @@ $settings['nav'][1]['url'] = '/about';
 $settings['nav'][2]['name'] = 'Donate';
 $settings['nav'][2]['url'] = '/donate';
 
+$settings['letters'][0]['heading'] = "I was misgendered by the staff.";
+$settings['letters'][0]['name'] = "misgendered";
+$settings['letters'][1]['heading'] = "An intentionally transphobic incident occurred.";
+$settings['letters'][1]['name'] = "tranphobia";
+$settings['letters'][2]['heading'] = "Their paperwork/computer system is not inclusive.";
+$settings['letters'][2]['name'] = "inclusion";
+$settings['letters'][3]['heading'] = "I was denied access to toilets/changing rooms/etc.";
+$settings['letters'][3]['name'] = "facilities";
+
 ?>
