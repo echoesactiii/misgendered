0202ffad55bf912f2402fde5f41777bd3fe86091
Kat <kat@polypod.me>
2015-10-09 15:49:17 +0100

    Restarting project with simpler scope, redesign using Mustache and latest Bootstrap

--- index.php
+++ index.php
@@ -1,63 +1,28 @@
 <?php
-	$page = preg_replace("/\//", '', preg_replace("/\/pages\//", '', $_SERVER['REQUEST_URI']));
-	if(!$page){ $page = "home"; }
-	$content = @file_get_contents("pages/".$page.".inc");
-	if(!$content){
-		header("HTTP/1.0 404 Not Found");
-		$content = file_get_contents("pages/404.inc");
-	}
-?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>you.misgendered.me</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="/style.css" rel="stylesheet" media="screen">
-		<link href="/typeahead-bs.css" rel="stylesheet" media="screen">
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
-		<script src="/hogan.js"></script>
-		<script src="/typeahead.min.js"></script>
-		<script src="/site.js"></script>
-	</head>
-	<body>
-		<div class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container">
-			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-			    </button>
+require "settings.php";
+require "include/rb.php";
+require "vendor/autoload.php";
 
-				<a href="/pages/home" class="navbar-brand">you.misgendered.me</a>
+R::setup('mysql:host='.$settings['database']['server'].'; dbname='.$settings['database']['database'],$settings['database']['username'],$settings['database']['password']);
 
-			    <div class="nav-collapse collapse navbar-responsive-collapse">
-					<ul class="nav navbar-nav">
-				        <li><a href="/pages/home">Home</a></li>
-				        <li><a href="/pages/about">About</a></li>
-				        <li><a href="/pages/helpout">Help out!</a></li>
-			    	</ul>
-			    </div>
-		    </div>
-	    </div>
-	    <?php
-	    	if($page == "home"){
-	    		echo file_get_contents("pages/home-hero.inc");
-	    	}
-	    ?>
-		<div>
-		<?php
-			echo $content;
-		?>
-		</div>
-		<footer class="bs-footer">
-			<a rel="license" href="http://creativecommons.org/licenses/by-sa/2.0/uk/deed.en_GB"><img alt="Creative Commons Licence" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/2.0/uk/88x31.png" /></a>
-			<a href="https://twitter.com/misgendered" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @misgendered</a>
-			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-			<iframe src="http://ghbtns.com/github-btn.html?user=cmantito&amp;repo=misgendered.me&amp;type=watch&amp;size=large" allowtransparency="true" frameborder="0" scrolling="0" width="90" height="30"></iframe>
-			<br/>This work by <a xmlns:cc="http://creativecommons.org/ns#" href="http://misgendered.me" property="cc:attributionName" rel="cc:attributionURL">misgendered.me</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/2.0/uk/deed.en_GB">Creative Commons Attribution-ShareAlike 2.0 UK: England &amp; Wales License</a>.
-		</footer>
-	</bodyL
-</html>
+$url = preg_replace("/\//", '', $_SERVER['REQUEST_URI']);
+if(!$url){ $url = "home"; }
+
+$m = new Mustache_Engine(array(
+	'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/mustache_templates')
+));
+
+$header = $m->loadTemplate("header");
+$footer = $m->loadTemplate("footer");
+
+$headerModel = array(
+	"site_title" => $settings['site']['title']
+);
+
+if($url == "home"){
+	$bodyModel = $headerModel;
+
+	$body = $m->loadTemplate("home");
+	echo $body->render($bodyModel);
+}
--- settings.php
+++ settings.php
@@ -1,8 +1,10 @@
 <?php
 
-$dbPassword = "";
-$dbUser = "";
-$dbName = "";
-$dbHost = "";
+$settings['database']['password'] = "918genesis11";
+$settings['database']['username'] = "root";
+$settings['database']['database'] = "misgenderedme";
+$settings['database']['server'] = "127.0.0.1";
+
+$settings['site']['title'] = "misgendered.me";
 
 ?>
